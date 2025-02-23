59d3d4fabcdb0fa91064bf7e4028b527bf689585
Noomnim <rueangyotnoom@gmail.com>
2025-02-23 13:31:22 +0700

    Use scope session for runing deploy one time for our entire test session

--- tests/test_favorites.py
+++ tests/test_favorites.py
@@ -1,20 +1,23 @@
 from script.deploy import deploy_favorites
 import pytest 
 
-@pytest.fixure(scope="function")
+@pytest.fixure(scope="session")
 def favorites_contract():
-    return deploy_favorites()
+    favorites_contract = deploy_favorites()
+    return favorites_contract
 
-def test_starting_values(favorites_contract): # Ok! Deploy the favorites contract
+# favorites_contract
+
+def test_starting_values(favorites_contract): # Ok! let's grab the contract, we already deployed!
     assert favorites_contract.retrieve() == 77
 
-def test_can_change_values(favorites_contract): # Ok! Deploy the favorites contract
+def test_can_change_values(favorites_contract): # Ok! let's grab the contract, we already deployed!
     # Act
     favorites_contract.store(42)
     # Assert
     assert favorites_contract.retrieve() == 42
 
-def test_can_add_people(favorites_contract): # Ok! Deploy the favorites contract
+def test_can_add_people(favorites_contract): # Ok! let's grab the contract, we already deployed!
     # Arrange
     new_person = "Becca"
     favorites_number = 16
