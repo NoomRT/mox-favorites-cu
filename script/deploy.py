56f9720765bdfcf57d512ad9d42d1deb5c75c3cf
Noomnim <rueangyotnoom@gmail.com>
2025-02-23 15:58:16 +0700

    Deploy and verify smart contract on Sepolia network at Blockscout.

--- script/deploy.py
+++ script/deploy.py
@@ -1,5 +1,6 @@
 from src import favorites
 from moccasin.boa_tools import VyperContract
+from moccasin.config import get_active_network
 
 def deploy_favorites() -> VyperContract:
     favorites_contract: VyperContract = favorites.deploy()
@@ -10,6 +11,10 @@ def deploy_favorites() -> VyperContract:
     ending_number: int = favorites_contract.retrieve()
     print(f"Ending number is {ending_number}")
 
+    active_network = get_active_network()
+    if active_network.has_explorer():
+        result = active_network.moccasin_verify(favorites_contract)
+        result.wait_for_verification()
     return favorites_contract
 
 def moccasin_main() -> VyperContract:
