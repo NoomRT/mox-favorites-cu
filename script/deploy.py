43dc85c21d9d096de6a8f78e0fe9a53120f70304
Noomnim <rueangyotnoom@gmail.com>
2025-02-23 15:12:40 +0700

    Use type mints

--- script/deploy.py
+++ script/deploy.py
@@ -1,15 +1,16 @@
 from src import favorites
+from moccasin.boa_tools import VyperContract
 
-def deploy_favorites():
-    favorites_contract = favorites.deploy()
-    starting_number = favorites_contract.retrieve()
+def deploy_favorites() -> VyperContract:
+    favorites_contract: VyperContract = favorites.deploy()
+    starting_number: int = favorites_contract.retrieve()
     print(f"Starting number is: {starting_number}")
 
     favorites_contract.store(77)
-    ending_number = favorites_contract.retrieve()
+    ending_number: int = favorites_contract.retrieve()
     print(f"Ending number is {ending_number}")
 
     return favorites_contract
 
-def moccasin_main():
-    deploy_favorites()
+def moccasin_main() -> VyperContract:
+    return deploy_favorites()
