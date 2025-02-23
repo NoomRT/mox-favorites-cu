036218d4f1d827addbc87894461678cd0404f9db
Noomnim <rueangyotnoom@gmail.com>
2025-02-23 16:18:56 +0700

    How to verify contracts that have already been deployed on a blockchain.

--- script/deploy.py
+++ script/deploy.py
@@ -3,15 +3,16 @@ from moccasin.boa_tools import VyperContract
 from moccasin.config import get_active_network
 
 def deploy_favorites() -> VyperContract:
-    favorites_contract: VyperContract = favorites.deploy()
-    starting_number: int = favorites_contract.retrieve()
-    print(f"Starting number is: {starting_number}")
+    # favorites_contract: VyperContract = favorites.deploy()
+    # starting_number: int = favorites_contract.retrieve()
+    # print(f"Starting number is: {starting_number}")
 
-    favorites_contract.store(77)
-    ending_number: int = favorites_contract.retrieve()
-    print(f"Ending number is {ending_number}")
+    # favorites_contract.store(77)
+    # ending_number: int = favorites_contract.retrieve()
+    # print(f"Ending number is {ending_number}")
 
     active_network = get_active_network()
+    favorites_contract = favorites.at("paste deployed contract address vyper smart contract here")
     if active_network.has_explorer():
         result = active_network.moccasin_verify(favorites_contract)
         result.wait_for_verification()
