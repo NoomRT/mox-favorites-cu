b4144f3fae80f41914cc3aa78794a55fade6487c
Noomnim <rueangyotnoom@gmail.com>
2025-02-23 12:55:58 +0700

    Tests using the Arrange, Act, Assert (AAA) framework.

--- tests/test_favorites.py
+++ tests/test_favorites.py
@@ -5,6 +5,21 @@ def test_starting_values():
     assert favorites_contract.retrieve() == 77
 
 def test_can_change_values():
+    # Arrange
     favorites_contract = deploy_favorites()
+    # Act
     favorites_contract.store(42)
+    # Assert
     assert favorites_contract.retrieve() == 42
+
+def test_can_add_people():
+    # Arrange
+    new_person = "Becca"
+    favorites_number = 16
+    favorites_contract = deploy_favorites()
+
+    # Act
+    favorites_contract.add_person(new_person, favorites_number)
+
+    # Assert
+    assert favorites_contract.list_of_people(0) == (favorites_number, new_person)
